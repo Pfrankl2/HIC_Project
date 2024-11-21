f40b68371904a03195278b8374b2ec0d2a880e92
JOE <jdemore4@kent.edu>
2024-11-21 18:28:14 -0500

    LETS TRY
    oh..sheeii...tt....

---
+++ HIC_Project/Assets/Scripts/FavoriteItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class FavoriteItem : MonoBehaviour
+{
+    public bool isFavorite_;
+    public string name_;
+}
