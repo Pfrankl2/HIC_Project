e3717773b744aa0ebc76a873c1e0ca2e0e5e8c4b
Pfrankl2 <pfrankl2@kent.edu>
2024-11-28 12:31:41 -0500

    Added the "Pixel Perfect" component to the main canvas
    The project now looks much crisper when being run! I am still using the iphone image we have been using instead of the simulator since the simulated one is much longer than our image. We can talk about this, but for right now it is looking a lot better!

--- HIC_Project/Assets/Scripts/BusDataModel.cs
+++ HIC_Project/Assets/Scripts/BusDataModel.cs
@@ -12,6 +12,8 @@ public class BusStop
     public string address;
     public float x;
     public float y;
+    // Add a "mapID" variable to define which map the bus stop
+    // should be displayed on (Main Campus / Dix Stadium).
 }
 
 // Class definition for Bus Route information.
